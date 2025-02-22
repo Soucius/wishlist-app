50abc1366f2cf14a188b345de9bfffa0c49c880b
Soucius <soucius.ggwp@gmail.com>
2025-02-22 15:28:19 +0300

    setting up two-way binding

--- src/app/wish-filter/wish-filter.component.ts
+++ src/app/wish-filter/wish-filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
 import { WishItem } from 'src/shared/models/wishItem';
 
 const filters = [
@@ -13,15 +13,17 @@ const filters = [
   styleUrls: ['./wish-filter.component.css']
 })
 export class WishFilterComponent implements OnInit {
-  @Output() filter = new EventEmitter<any>();
+  @Input() filter: any;
+  @Output() filterChange = new EventEmitter<any>();
 
   ngOnInit(): void {
-    this.filter.emit(filters[0]);
+    this.updateFilter('0');
   }
 
   listFilter: any = '0';
 
-  changeFilter(value: any) {
-    this.filter.emit(filters[value]);
+  updateFilter(value: any) {
+    this.filter = filters[value];
+    this.filterChange.emit(this.filter);
   }
 }
